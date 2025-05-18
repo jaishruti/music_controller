851f27e770c1b8d753cc2e12631d8138c1950b4f
jaishruti <shrutijaiswal223@gmail.com>
2025-05-18 19:23:45 +0530

    RoomView added in urls

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import main
+from .views import RoomView
 
 urlpatterns = [
-    path('home',main),
-    path('',main),
+    path('home',RoomView.as_view()),
+    # path('',main),
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .serializers import RoomSerializer
+from .serializers import RoomSerializers
 from .models import Room
 
 class RoomView(generics.CreateAPIView):     #allows to create and view rooms
     queryset = Room.objects.all()
-    serializer_class = RoomSerializer
-    
+    serializer_class = RoomSerializers
