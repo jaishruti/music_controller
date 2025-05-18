e84eff49010a83b09183127a472efb865f094e60
jaishruti <shrutijaiswal223@gmail.com>
2025-05-18 19:17:24 +0530

    Room view added in views api

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 
-# Create your views here.
-def main(request):
-    return HttpResponse("<h1>Hello</h1>")
+class RoomView(generics.CreateAPIView):     #allows to create and view rooms
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+    
