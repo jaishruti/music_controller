0d0d4df6822719dadf9e9e12a1e733469ce986f6
jaishruti <shrutijaiswal223@gmail.com>
2025-05-18 21:41:04 +0530

    frontend urls and views added

--- music_controller/urls.py
+++ music_controller/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('api.urls')),
+    path('',include('frontend.urls')),
     path('api/',include('api.urls'))
 ]
