70a85f3cc4701b6ce74d0437e36fdea3ceda4fb8
jaishruti <shrutijaiswal223@gmail.com>
2025-05-23 18:15:35 +0530

    api for User in room to directly take to room page when already joined a room

--- api/urls.py
+++ api/urls.py
@@ -4,11 +4,14 @@ from .views import  RoomView
 from .views import CreateRoomView
 from .views import GetRoom
 from .views import JoinRoom
+from .views import UserInRoom
 
 urlpatterns = [
     path('home',RoomView.as_view()),
     path('create-room',CreateRoomView.as_view()),
     path('get-room',GetRoom.as_view()),
-    path('join-room',JoinRoom.as_view())
+    path('join-room',JoinRoom.as_view()),
+    path('user-in-room',UserInRoom.as_view())
+
     # path('',main),
 ]
