99c54f69a5912934e8ef559626495672367533ac
jaishruti <shrutijaiswal223@gmail.com>
2025-05-23 13:17:10 +0530

    backend api view and url created for join room

--- api/views.py
+++ api/views.py
@@ -20,6 +20,25 @@ from django.shortcuts import get_object_or_404
 from .models import Room
 from .serializers import RoomSerializers
 
+class JoinRoom(APIView):
+    serializer_class = RoomSerializers
+    lookup_url_kwargs = 'code'
+    def post(self, request, format=None):
+        code = request.GET.get(self.lookup_url_kwargs)
+        if not code:
+            return Response({"error":"Bad Request: code not found in request"}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            room = Room.objects.get(code=code)
+        except Room.DoesNotExist:
+            return Response({"error":"Bad Request: invalid code"}, status=status.HTTP_400_BAD_REQUEST)
+        data = RoomSerializers(room).data
+        if not request.session.session_key:
+            request.session.save()
+
+        #save the roomcode for that session inorder the know user had joined which room
+        self.request.session['room_code'] = code
+        return Response({"message":'Room Joined'}, status= status.HTTP_200_OK)
+    
 class GetRoom(APIView):
     serializer_class = RoomSerializers
     lookup_url_kwarg = 'code'
@@ -31,9 +50,17 @@ class GetRoom(APIView):
 
         room = get_object_or_404(Room, code=code)
         data = RoomSerializers(room).data
-        print(data)
+        print(f"Received request with code: {code}")
+        print(f"Serialized room data: {data}")  
+        if not request.session.session_key:
+            request.session.save()
         data['is_host'] = request.session.session_key == room.host
 
+        #normalize the API response key
+        data["votesToSkip"] = data.pop("votes_to_skip", 2)
+        data["guestCanPause"] = data.pop("guest_can_pause", False)
+
+        print(data)
         return Response(data, status=status.HTTP_200_OK)
 
 class CreateRoomView(APIView):  #APIView overrides the methods 
@@ -54,10 +81,12 @@ class CreateRoomView(APIView):  #APIView overrides the methods
                 room = queryset[0]
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
+                self.request.session['room_code'] = room.code
                 room.save(update_fields=['guest_can_pause','votes_to_skip'])
                 return Response(RoomSerializers(room).data, status=status.HTTP_200_OK)
             else:
                 room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
+                self.request.session['room_code'] = room.code
                 room.save()
             #srialixe the room object and pass the data as response
                 return Response(RoomSerializers(room).data, status=status.HTTP_201_CREATED)       
